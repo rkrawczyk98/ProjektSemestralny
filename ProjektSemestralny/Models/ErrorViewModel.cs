d28264922400a8e95ea916fad776315882397139
Rafał Krawczyk <rafal.krawczyk@microsoft.wsei.edu.pl>
2023-02-11 14:26:45 +0100

    Add project files.

---
+++ ProjektSemestralny/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjektSemestralny.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
