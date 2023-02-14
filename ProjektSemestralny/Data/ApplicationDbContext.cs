f1b4ea9a9ae4b9bcd76b525149f8ecc966ac09b3
Demax123123 <danielwlodek522@gmail.com>
2023-02-14 08:23:14 +0100

    Ankieta

---
+++ ProjektSemestralny/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjektSemestralny.Models;
+
+namespace ProjektSemestralny.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Ankiety> Ankiety { get; set; }
+    }
+}
---
+++ ProjektSemestralny/Models/Ankiety.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjektSemestralny.Models
+{
+    public class Ankiety
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nazwa { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime UtworzoneDateTime { get; set; } = DateTime.Now;
+    }
+}
